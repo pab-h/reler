1bbd7fdcd084dbcc7662d6fa811bf5ad0890750e
pab-h <dev.pab.2020@gmail.com>
2024-09-12 18:36:18 -0300

    feat: token entity, token types enum and token erros

---
+++ reler/token.py
@@ -0,0 +1,31 @@
+from enum import Enum
+from enum import auto
+
+class UnrecognizedTokenError(Exception):
+    pass
+
+class MalformedTokenError(Exception):
+    pass
+
+class TokenTypes(Enum):
+    REL = auto()
+    LER = auto()
+    ODO = auto()
+    ESE = auto()
+    RELER = auto()
+    LITERAL = auto()
+    NUMBER = auto()
+    GREATERTHAN = auto()
+    WHITESPACE = auto()
+    IDENTIFIER = auto()
+    DEIFIED = auto()
+    ASSIGN = auto()
+    MINUS = auto()
+
+class Token:
+    def __init__(self, value, _type: TokenTypes) -> None:
+        self.value = value
+        self.type = _type
+
+    def __repr__(self) -> str:
+        return f"Token({ self.value }, { self.type.name })"
